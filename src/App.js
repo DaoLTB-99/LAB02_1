e6ac9a5aabec4bd9c1e856925f53428390ce6748
Bich Dao Le <bichdaole07@gmail.com>
2022-03-02 08:49:41 +0700

    second commit LAB02_2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
+import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
 class App extends Component {
-
   render() {
     return (
       <div className="App">
-        <p>Phiên bản React hiện tại trên máy của bạn là: {React.version}</p>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
       </div>
     );
   }
